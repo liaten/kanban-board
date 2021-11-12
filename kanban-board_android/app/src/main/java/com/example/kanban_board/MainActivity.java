2f70a5d6fe922ad183e2c0ade06440a3ffe05a1f
Igor G <goncharovigor12@gmail.com>
2021-11-12 12:30:19 +0300

    “Подключена_Firebase”

--- kanban-board_android/app/src/main/java/com/example/kanban_board/MainActivity.java
+++ kanban-board_android/app/src/main/java/com/example/kanban_board/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -51,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 showMainWindow();
             }
         });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
 
     }
 
