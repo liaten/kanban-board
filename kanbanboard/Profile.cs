5605fcece1f04b05570c689436192ea4a362e987
liaten <vlad171vlad@gmail.com>
2021-11-12 19:44:26 +0300

    .gitignore is now working

---
+++ kanbanboard/Profile.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace kanbanboard
+{
+    public partial class Profile : Form
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kanbanboard/Tasks.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace kanbanboard
+{
+    public partial class Tasks : Form
+    {
+        public Tasks()
+        {
+            InitializeComponent();
+        }
+    }
+}
