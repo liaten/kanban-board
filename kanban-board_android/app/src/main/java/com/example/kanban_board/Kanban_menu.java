d42c4fe555b2396a3cdbc022d2ce0378db18e70e
Igor G <goncharovigor12@gmail.com>
2021-11-13 14:59:42 +0300

    “Небольшие_изменения_в_стилях”

--- kanban-board_android/app/src/main/java/com/example/kanban_board/Kanban_menu.java
+++ kanban-board_android/app/src/main/java/com/example/kanban_board/Kanban_menu.java
@@ -6,16 +6,25 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Kanban_menu extends AppCompatActivity {
 
+    Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kanban_menu);
+
+        logout = findViewById(R.id.LogOut);
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         //NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
@@ -23,6 +32,8 @@ public class Kanban_menu extends AppCompatActivity {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
 
     }
 }
