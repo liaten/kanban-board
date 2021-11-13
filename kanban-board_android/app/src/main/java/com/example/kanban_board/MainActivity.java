5a9a8563dd92cbe5e2737da66b56b2e58e726490
dsyt17 <90345283+dsyt17@users.noreply.github.com>
2021-11-13 20:40:46 +0300

    Стили для кнопок и view (#18)

--- kanban-board_android/app/src/main/java/com/example/kanban_board/MainActivity.java
+++ kanban-board_android/app/src/main/java/com/example/kanban_board/MainActivity.java
@@ -7,14 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Layout;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kanban_board.Models.User;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -54,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
         root = findViewById(R.id.main_layout);
 
+
         //Кнопки
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
@@ -77,7 +82,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
     }
+
 
     //Войти
 
