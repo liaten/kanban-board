dd456860ff3cd141a4f9d0762eb8e689eb6cae23
Igor G <goncharovigor12@gmail.com>
2021-11-30 14:46:40 +0300

    “Тикеты_андроид_работают”

--- kanban-board_android/app/src/main/java/com/example/kanban_board/Calendar.java
+++ kanban-board_android/app/src/main/java/com/example/kanban_board/Calendar.java
@@ -1,21 +1,34 @@
 package com.example.kanban_board;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kanban_board.Models.MyAdapterForCalendar;
 import com.example.kanban_board.Models.Ticket;
 import com.example.kanban_board.Models.User;
+import com.example.kanban_board.Models.UserNew;
+import com.google.android.gms.common.server.converter.StringToIntConverter;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
@@ -26,6 +39,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +57,19 @@ public class Calendar extends Fragment {
     DatabaseReference database;
     MyAdapterForCalendar adapter;
     ArrayList<Ticket> list;
+
+    Spinner spinner;
+    DatabaseReference dbBoards;
+    String boardName = "";
+
+    Button addBoardBtn;
+    LinearLayout root;
+    DatabaseReference boards;
+
+    Button addTicketBtn;
+    DatabaseReference tickets;
+
+    TextView Header;
 
 
     public Calendar() {
@@ -73,9 +100,153 @@ public class Calendar extends Fragment {
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_calendar, container, false);
 
         recyclerView = (RecyclerView) viewGroup.findViewById(R.id.ticketList);
-        database = FirebaseDatabase.getInstance().getReference("newboard");
+        spinner = (Spinner) viewGroup.findViewById(R.id.spinnerBoards);
+        addBoardBtn = (Button) viewGroup.findViewById(R.id.addBoardBtn);
+        addTicketBtn = (Button) viewGroup.findViewById(R.id.addTicketBtn);
+        root = (LinearLayout) viewGroup.findViewById(R.id.calendar_root_layout);
+        Header = (TextView) viewGroup.findViewById(R.id.headerCalender);
+
+        GetBoards();
+        GetTicketsFromSpinner();
+        AddBoard();
+        AddTicket();
+
+
+
+
+
+
+        return viewGroup;
+        //return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+
+
+
+    private void AddTicket() {
+
+        addTicketBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder addT = new AlertDialog.Builder(getActivity());
+                addT.setTitle("Добавление нового тикета");
+                addT.setMessage("Введите название тикета");
+
+                LayoutInflater inflater = LayoutInflater.from(getActivity());
+                View addT_window = inflater.inflate(R.layout.add_new_ticket, null);
+                addT.setView(addT_window);
+
+                final MaterialEditText name = addT_window.findViewById(R.id.addTicketName);
+                final MaterialEditText text = addT_window.findViewById(R.id.addTicketText);
+                final MaterialEditText people = addT_window.findViewById(R.id.addTicketPeople);
+                final MaterialEditText status = addT_window.findViewById(R.id.addTicketStatus);
+
+                addT.setNegativeButton("Назад", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                addT.setPositiveButton("Отправить", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        if (TextUtils.isEmpty(name.getText().toString())){
+                            Snackbar.make(root, "Введите название!", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (TextUtils.isEmpty(text.getText().toString())){
+                            Snackbar.make(root, "Введите текст тикета!", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (TextUtils.isEmpty(people.getText().toString())){
+                            Snackbar.make(root, "Введите разработчиков!", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (TextUtils.isEmpty(status.getText().toString())){
+                            Snackbar.make(root, "Введите статус!", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        }
+
+
+                        String ticketName = name.getText().toString();
+                        String ticketText = text.getText().toString();
+                        String ticketPeople = people.getText().toString();
+                        String ticketStatus = status.getText().toString();
+                        String ticketBoardName = spinner.getSelectedItem().toString();
+
+                        Ticket ticket = new Ticket(ticketPeople, ticketText, ticketName);
+
+
+                        FirebaseDatabase db = FirebaseDatabase.getInstance();
+                        tickets = db.getReference(ticketBoardName);
+
+                        tickets.child(ticketName).setValue(ticket);
+
+
+                    }
+                });
+
+                addT.show();
+
+            }
+        });
+
+
+    }
+
+    private void GetTicketsFromSpinner() {
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent,
+                                       View itemSelected, int selectedItemPosition, long selectedId) {
+                GetTickets();
+                Header.setText("Доска " + boardName);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    private void GetBoards() {
+
+        dbBoards = FirebaseDatabase.getInstance().getReference("boards");
+        dbBoards.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                final List<String> boardsList = new ArrayList<String>();
+
+                for (DataSnapshot areaSnapshot : snapshot.getChildren()) {
+                    String boardNameS = areaSnapshot.getValue(String.class);
+                    boardsList.add(boardNameS);
+            }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, boardsList);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(arrayAdapter);
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+    //Тикеты в спинер
+    private void GetTickets() {
+
+        boardName = spinner.getSelectedItem().toString();
+
+        database = FirebaseDatabase.getInstance().getReference(boardName);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
 
         list = new ArrayList<>();
         adapter = new MyAdapterForCalendar(getActivity(), list);
@@ -97,16 +268,76 @@ public class Calendar extends Fragment {
             }
         });
 
-
-
-
-
-
-
-        return viewGroup;
-        //return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
 
+    private void AddBoard(){
+
+        addBoardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder addB = new AlertDialog.Builder(getActivity());
+                addB.setTitle("Добавление новой доски");
+                addB.setMessage("Введите название доски");
+
+                LayoutInflater inflater = LayoutInflater.from(getActivity());
+                View addB_window = inflater.inflate(R.layout.add_new_board, null);
+                addB.setView(addB_window);
+
+                final MaterialEditText name = addB_window.findViewById(R.id.addBoardName);
+
+                addB.setNegativeButton("Назад", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                addB.setPositiveButton("Отправить", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                if (TextUtils.isEmpty(name.getText().toString())){
+                    Snackbar.make(root, "Введите название!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                        String boardName = name.getText().toString();
+
+
+                        DatabaseReference itemsRef = FirebaseDatabase.getInstance().getReference().child("boards");
+                        ValueEventListener eventListener = new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                                    String increment = ds.getKey();
+
+                                    Integer myNum = Integer.parseInt(increment) + 1;
+
+                                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                                    boards = db.getReference("boards").child(String.valueOf(myNum));
+                                    boards.setValue(boardName);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        };
+                        itemsRef.addListenerForSingleValueEvent(eventListener);
+
+
+                    }
+                });
+
+                addB.show();
+
+
+            }
+        });
+
+
+
+    }
 
 
 }
--- kanban-board_android/app/src/main/java/com/example/kanban_board/Models/Ticket.java
+++ kanban-board_android/app/src/main/java/com/example/kanban_board/Models/Ticket.java
@@ -5,7 +5,17 @@ public class Ticket {
     private String People;
     private String Ticket;
     private String Title;
-    public String status;
+    private String status;
+    private String boards;
+
+    public Ticket() {
+    }
+
+    public Ticket(String People, String Ticket, String Title) {
+        this.People = People;
+        this.Ticket = Ticket;
+        this.Title = Title;
+    }
 
     public String getpeople() {
         return People;
@@ -17,5 +27,34 @@ public class Ticket {
 
     public String gettitle() {
         return Title;
+    }
+
+
+    public void setPeople(String People) {
+        this.People = People;
+    }
+
+    public void setTicket(String Ticket) {
+        this.Ticket = Ticket;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBoards() {
+        return boards;
+    }
+
+    public void setBoards(String boards) {
+        this.boards = boards;
     }
 }
